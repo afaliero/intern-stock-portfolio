ad125a0db2b6917db3b3ed800db30ffb3b88cb0f
Anna Faliero <afaliero@berkeley.edu>
2020-06-26 09:57:28 -0400

    added sara's changes

--- src/app/search-box/search-box.component.ts
+++ src/app/search-box/search-box.component.ts
@@ -1,34 +1,37 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Stock } from '../stock';
 import { STOCKS } from '../portfolio-stocks';
 import { StocksComponent } from '../stocks/stocks.component';
 import { StockDataService } from '../stock-data.service';
+import { Stock } from '../stock';
 
+export var EXTRA_STOCKS: Stock[] = [];
 @Component({
   selector: 'app-search-box',
   templateUrl: './search-box.component.html',
   styleUrls: ['./search-box.component.css']
 })
 
-export class SearchBoxComponent extends StocksComponent implements OnInit {
+export class SearchBoxComponent implements OnInit {
 
   ngOnInit(): void {
-   // STOCKS.sort((a, b) => (a.returns > b.returns)? 1 : -1);
   }
 
-  addTodo(value: string) {
-    var len = STOCKS.push({
+  constructor(private stockApi: StockDataService) {
+
+  }
+
+  addTodo(value: string) {    
+    var len = EXTRA_STOCKS.push({
       ticker: value,
       price: 0,
-      company: "X",
+      company: value,
       returns: -100,
       june15: 100000,
       marketCap: 0,
-      logo: ''
+      logo: null,
     })
-    //var stockApi = new StockDataService;
-    //stockApi.callApi(len - 1);
-  }
+    this.stockApi.callApi(EXTRA_STOCKS[len - 1]);
+  } 
 
 
 }
